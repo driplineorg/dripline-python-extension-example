f078d6a009d8e1a47db72c056737ee29507c293b
Ben LaRoque <benjamin.laroque@gmail.com>
2019-10-15 16:43:14 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_namespace_packages
+
+def set_version_data():
+    import subprocess
+    import re
+    version_data = {}
+    pwd = subprocess.check_output(['pwd'])
+    print("pwd: {}".format(pwd))
+    origin = subprocess.check_output(['git', 'remote', 'get-url', 'origin'])
+    print("raw origin: '{}'".format(origin))
+
+packages = find_namespace_packages('.', include=['dripline*'])
+print('packages are: {}'.format(packages))
+
+setup(
+    name="kv_plugin",
+    setup_requires=['setuptools_scm'],
+    use_scm_version={
+        "root": ".",
+        "write_to": "./version.py",
+    },
+    packages=packages,
+)
